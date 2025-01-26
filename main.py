6dc29e93218b193b6f2753220bd7a31f8249380e
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-26 14:00:04 -0700

    setting up word set and hangman art dict

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-def test():
-    pass
+import random
+
+words = ("apple", "banana", "orange", "coconut", "pinapple")
+
+#dict of key:()
+hangman_art = { 0: ("   ",
+                    "   ",
+                    "   "),
+                1: (" o ",
+                    "   ",
+                    "   "),
+                2: (" o ",
+                    " | ",
+                    "   "),
+                3: (" o ",
+                    "/| ",
+                    "   "),
+                4: (" o ",
+                    "/|\\",
+                    "   "),
+                5: (" o ",
+                    "/|\\",
+                    "/  "),
+                6: (" o ",
+                    "/|\\",
+                    "/ \\")}
