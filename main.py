1768978f4c611c98dc05f1dd5d09061a31a55682
Lincoln Bunker <webdev.lincolnbunker@gmail.com>
2025-01-26 14:27:13 -0700

    creating a bigger word set and adding all the functions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
-
-words = ("apple", "banana", "orange", "coconut", "pinapple")
+from words import words
 
 #dict of key:()
 hangman_art = { 0: ("   ",
@@ -24,3 +23,58 @@ hangman_art = { 0: ("   ",
                 6: (" o ",
                     "/|\\",
                     "/ \\")}
+
+def display_man(wrong_guesses):
+    print("**********")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("**********")
+
+def display_hint(hint):
+    print(" ".join(hint))
+
+def display_answer(answer):
+    print(" ".join(answer))
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a Letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid Input!")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!")
+            is_running = False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE TAKE THE L")
+            is_running = False
+
+if __name__ == "__main__":
+    main()
---
+++ words.py
@@ -0,0 +1,13 @@
+#words for hangman game
+
+words = (
+    "example", "challenge", "solution", "mountain", "sunshine", "umbrella",
+    "horizon", "butterfly", "journey", "mystery", "adventure", "paradise",
+    "festival", "beautiful", "landscape", "painting", "treasure", "memories",
+    "fantastic", "happiness", "friendship", "champion", "learning", "creativity",
+    "discovery", "resource", "education", "playground", "freedom", "ambition",
+    "kindness", "compassion", "strength", "wonderful", "celebrate", "daydream",
+    "imagine", "brilliant", "artistic", "prosperity", "appreciate", "explorer",
+    "community", "harmony", "inspiring", "dedicated", "innovate", "resilient",
+    "enthusiasm", "gratitude"
+)
